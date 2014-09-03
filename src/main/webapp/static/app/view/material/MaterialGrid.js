918952d1c840cb0b6207d34cc4947e1a2bed52b0
cgx1844568 <cgx1844568@P1844568A218.bydhq.com.cn>
2014-09-03 09:57:37 +0800

    大改备份

--- src/main/webapp/static/app/view/material/MaterialGrid.js
+++ src/main/webapp/static/app/view/material/MaterialGrid.js
@@ -8,44 +8,17 @@
 Ext.define('App.view.material.MaterialGrid', {
     extend: 'Ext.grid.Panel',
     alias: 'widget.materialGrid',
-    frame: true,
+    frame: false,
     store: 'simpsonsStore',
-    tbar: [{
-            xtype: 'button',
-            text: '新增',
-            iconCls: 'icon-add'
-//            handler: add
-        }, {
-            xtype: 'button',
-            text: '删除',
-            iconCls: 'icon-delete'
-//            handler: alter
-        }, {
-            xtype: 'button',
-            text: '保存',
-            iconCls: 'icon-save'
-//            handler: otherDelete
-        }, {
-            xtype: 'button',
-            text: '导出',
-            iconCls: 'icon-excel'
-//            handler: otherDelete
-        }],
+    tbar: [],
     dock: 'top,bottom',
     columns: [
-        { text: 'Name',  dataIndex: 'name' },
-        { text: 'Email', dataIndex: 'email', flex: 1 },
-        { text: 'Phone', dataIndex: 'phone' }
+        {text: 'Name', dataIndex: 'name'},
+        {text: 'Email', dataIndex: 'email', flex: 1},
+        {text: 'Phone', dataIndex: 'phone'}
     ],
     columnLines: true,
-//    selModel: Ext.create('Ext.selection.RowModel',{mode:"MULTI"}),
     forceFit: true,
-//    dockedItems: [{
-//            xtype: 'tbar',
-//            dock: 'bottom,top',
-//            displayInfo: true
-//        }],
-    buttons: [{text: '登陆'}, {text: '取消'}],
     initComponent: function() {
         Ext.create('Ext.data.Store', {
             storeId: 'simpsonsStore',
@@ -68,7 +41,13 @@ Ext.define('App.view.material.MaterialGrid', {
                 }
             }
         });
-        console.info('MaterialView.initComponent');
+        console.info('MaterialGrid.initComponent');
         this.callParent();
-    }
+    },
+    dockedItems: [{
+            xtype: 'pagingtoolbar',
+//            store: 'MaterialExceptionLogStore', // same store GridPanel is  using  
+            dock: 'top',
+            displayInfo: true
+        }]
 });
